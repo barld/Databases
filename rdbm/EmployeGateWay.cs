507eb166a2757b5297c8510ed30ccde5a97ba338
barld <bakeds@gmail.com>
2016-09-26 16:49:14 +0200

    first setup connection

---
+++ rdbm/EmployeGateWay.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rdbm
+{
+    public class EmployeGateWay
+    {
+        public static void GetAll()
+        {
+            var dt = new DataTable();
+            //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="C:\Users\barld\documents\visual studio 2015\Projects\rdbm\rdbm\Employe.mdf";Integrated Security=True
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;
+                          AttachDbFilename=C:\Users\barld\documents\visual studio 2015\Projects\rdbm\rdbm\Employe.mdf;
+                          Integrated Security=True;
+                          Connect Timeout=30;
+                          Integrated Security=True";
+            using (var cn = new SqlConnection(connectionString))
+            using (var cmd = new SqlCommand("Select * From [Employe]", cn))
+            {
+                cn.Open();
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    dt.Load(reader);
+                }
+            }
+        }
+    }
+}
