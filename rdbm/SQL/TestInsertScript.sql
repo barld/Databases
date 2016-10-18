bd545cc97aa32b22ff379b8dc9ed74862557d07c
barld <bakeds@gmail.com>
2016-10-18 22:15:53 +0200

    add scripts met testdata

---
+++ rdbm/SQL/TestInsertScript.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [Address] VALUES ('Netherlands', '4134BZ', '9a', 'Rotterdam', 'blablaStraat');
+INSERT INTO [HeadQuater] VALUES ('belangerijk', 1, 400, 'Netherlands', '4134BZ', '9a');
+INSERT INTO [Employee] VALUES (562323, 'Mama', 'Boot', 'belangerijk');
+INSERT INTO [Employee] VALUES (562324, 'Papa', 'Boot', 'belangerijk');
